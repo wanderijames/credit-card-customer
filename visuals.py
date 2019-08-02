432453a6a44fe762bd54b6263d45172c58c3af11
James Wanderi <wanderikinyanjui@gmail.com>
2019-08-02 16:21:30 +0300

    spectoral

--- visuals.py
+++ visuals.py
@@ -94,7 +94,40 @@ def cluster_results(reduced_data, preds, centers, pca_samples):
 	           s = 150, linewidth = 4, color = 'black', marker = 'x');
 
 	# Set plot title
-	ax.set_title("Cluster Learning on PCA-Reduced Data - Centroids Marked by Number\nTransformed Sample Data Marked by Black Cross");
+	ax.set_title("Cluster Learning on PCA-Reduced Data \nTransformed Sample Data Marked by Black Cross");
+
+def cluster_results(reduced_data, preds, pca_samples, centers=[]):
+    '''
+    Visualizes the PCA-reduced cluster data in two dimensions
+    Adds cues for cluster centers and student-selected sample data
+    '''
+
+    predictions = pd.DataFrame(preds, columns = ['Cluster'])
+    plot_data = pd.concat([predictions, reduced_data], axis = 1)
+
+    # Generate the cluster plot
+    fig, ax = plt.subplots(figsize = (14,8))
+
+    # Color map
+    cmap = ["purple", "brown", "green", "blue", "red", "magenta", "black", "yellow"]
+
+    # Color the points based on assigned cluster
+    for i, cluster in plot_data.groupby('Cluster'):   
+        cluster.plot(ax = ax, kind = 'scatter', x = 'Dimension 1', y = 'Dimension 2', \
+                     color=cmap[i], label = 'Cluster %i'%(i), s=30);
+
+    # Plot centers with indicators
+    for i, c in enumerate(centers):
+        ax.scatter(x = c[0], y = c[1], color = 'white', edgecolors = 'black', \
+                   alpha = 1, linewidth = 2, marker = 'o', s=200);
+        ax.scatter(x = c[0], y = c[1], marker='$%d$'%(i), alpha = 1, s=100);
+
+    # Plot transformed sample points 
+    ax.scatter(x = pca_samples[:,0], y = pca_samples[:,1], \
+               s = 150, linewidth = 4, color = 'black', marker = 'x');
+
+    # Set plot title
+    ax.set_title("Cluster Learning on PCA-Reduced Data - Centroids Marked by Number\nTransformed Sample Data Marked by Black Cross");
 
 
 def biplot(good_data, reduced_data, pca):
