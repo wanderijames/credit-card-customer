8434a1d38de7685228c20625cb35365ffd9d6724
James Wanderi <wanderikinyanjui@gmail.com>
2019-08-12 17:10:16 +0300

    submit draft

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ### Capstone project
 
+The data used can be downloaded from [Kaggle](https://www.kaggle.com/arjunbhasin2013/ccdata)
+
+Place that in the root directory as `ccdata.zip`
+
 #### Development Environment Setup
 1. Install `pipenv` as documented in their [official page](https://docs.pipenv.org/en/latest/)
 2. While in the root directory install the python virtual environment:
@@ -12,6 +16,10 @@
 
 	> pipenv install --dev
 
-If you want to use the pip for your setup, you generate the requirements.txt file:
+	If you want to use the pip for your setup, you generate the requirements.txt file:
 
 	> pipenv lock -r > requirements.txt
+
+4. Start the Jupyter notebook
+
+	> jupyter notebook credit-card-customer.ipynb
--- visuals.py
+++ visuals.py
@@ -10,12 +10,15 @@ from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline') # noqa
 ###########################################
 
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import numpy as np
 import pandas as pd
+from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_samples, silhouette_score
 
 # Color map
 CMAP: list = [
@@ -115,7 +118,10 @@ def cluster_results(reduced_data, preds, pca_samples, centers=[]):
         "Sample Data Marked by Black Cross")
 
 
-def biplot(good_data, reduced_data, pca):
+def biplot(
+        good_data: pd.DataFrame,
+        reduced_data: pd.DataFrame,
+        pca: PCA):
     '''
     Produce a biplot that shows a scatterplot of the reduced
     data and the projections of the original features.
@@ -160,7 +166,9 @@ def biplot(good_data, reduced_data, pca):
     return ax
 
 
-def compare_cluster_means(good_data_with_label):
+def compare_cluster_means(
+        good_data_with_label: pd.DataFrame):
+    """Compare the clusters"""
     scaler = MinMaxScaler()
     cluster_analysis_df = good_data_with_label\
         .groupby("Cluster")\
@@ -175,3 +183,92 @@ def compare_cluster_means(good_data_with_label):
     fontP.set_size('x-small')
     ax.legend(bbox_to_anchor=(0.9, 0.5), prop=fontP)
     return ax
+
+def kmeans_cluster_analysis(data: pd.DataFrame, range_n_clusters: list):
+    """Visualize clusters and their scores
+
+    :param data: Data for analysis
+    :param range_n_clusters: List of clusters for analisys
+    """  
+    X = data.copy()
+    for n_clusters in range_n_clusters:
+        # Create a subplot with 1 row and 2 columns
+        fig, (ax1, ax2) = plt.subplots(1, 2)
+        fig.set_size_inches(14, 4)
+
+        # The 1st subplot is the silhouette plot
+        # The silhouette coefficient can range from -1, 1 but in this example all
+        # lie within [-0.1, 1]
+        ax1.set_xlim([-0.1, 1])
+        # The (n_clusters+1)*10 is for inserting blank space between silhouette
+        # plots of individual clusters, to demarcate them clearly.
+        ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+
+        clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+        cluster_labels = clusterer.fit_predict(X)
+
+        # The silhouette_score gives the average value for all the samples.
+        # This gives a perspective into the density and separation of the formed
+        # clusters
+        silhouette_avg = silhouette_score(X, cluster_labels)
+        print("For n_clusters =", n_clusters,
+              "The average silhouette_score is :", silhouette_avg)
+
+        # Compute the silhouette scores for each sample
+        sample_silhouette_values = silhouette_samples(X, cluster_labels)
+
+        y_lower = 10
+        for i in range(n_clusters):
+            # Aggregate the silhouette scores for samples belonging to
+            # cluster i, and sort them
+            ith_cluster_silhouette_values = \
+                sample_silhouette_values[cluster_labels == i]
+
+            ith_cluster_silhouette_values.sort()
+
+            size_cluster_i = ith_cluster_silhouette_values.shape[0]
+            y_upper = y_lower + size_cluster_i
+
+            color = cm.nipy_spectral(float(i) / n_clusters)
+            ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                              0, ith_cluster_silhouette_values,
+                              facecolor=color, edgecolor=color, alpha=0.7)
+
+            # Label the silhouette plots with their cluster numbers at the middle
+            ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+            # Compute the new y_lower for next plot
+            y_lower = y_upper + 10  # 10 for the 0 samples
+
+        ax1.set_title("The silhouette plot for the various clusters.", fontsize=8)
+        ax1.set_xlabel("The silhouette coefficient values", fontsize=8)
+        ax1.set_ylabel("Cluster label", fontsize=8)
+
+        # The vertical line for average silhouette score of all the values
+        ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+        ax1.set_yticks([])  # Clear the yaxis labels / ticks
+        ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+        # 2nd Plot showing the actual clusters formed
+        colors = cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
+        ax2.scatter(X.loc[:, 'Dimension 1'], X.loc[:, 'Dimension 2'], marker='.', s=30, lw=0, alpha=0.7,
+                    c=colors, edgecolor='k')
+
+        # Labeling the clusters
+        centers = clusterer.cluster_centers_
+        # Draw white circles at cluster centers
+        ax2.scatter(centers[:, 0], centers[:, 1], marker='o',
+                    c="white", alpha=1, s=200, edgecolor='k')
+
+        for i, c in enumerate(centers):
+            ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1,
+                        s=50, edgecolor='k')
+
+        ax2.set_title("The visualization of the clustered data.", fontsize=8)
+        ax2.set_xlabel("Feature space for the 1st feature", fontsize=8)
+        ax2.set_ylabel("Feature space for the 2nd feature", fontsize=8)
+
+        plt.suptitle(("Silhouette analysis for K-Means clustering on sample data "
+                      "with n_clusters = {}".format(n_clusters)),
+                     fontsize=10, fontweight='bold')
